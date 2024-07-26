4e4a37830b1d2a03950accd1276faae29d4b979f
Saurabh Pundir <75524401+saurabhpundir05@users.noreply.github.com>
2024-07-26 23:00:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const categoryRoute=require('./api/routes/category');
+const blogRoute=require('./api/routes/blog');
+const userRoute=require('./api/routes/user')
+const bodyParser=require('body-parser');
+const fileUpload=require('express-fileupload');
+
+mongoose.connect('mongodb+srv://tobi:Yogi$456@cluster0.kcsiwae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(res=>{
+    console.log('mongodb  connected');
+})
+.catch(err=>{
+    console.log(err);
+})
+
+app.use(fileUpload({
+    useTempFiles:true
+}))
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use('/category',categoryRoute);
+app.use('/blog',blogRoute);
+app.use('/user',userRoute);
+
+app.get('*',(req,res)=>{
+    res.status(200).json({
+        msg:'bad request'
+    })
+})
+
+module.exports=app;
